49958e52f315b941f05b25599cfbba79b3388fd8
AProgrammerFemale <54490976+AProgrammerFemale@users.noreply.github.com>
2019-08-25 20:23:38 +0200

    Update Types.hpp

--- Basic/Types.hpp
+++ Basic/Types.hpp
@@ -1,13 +1,14 @@
 //Copyright Alice Framework, All Rights Reserved
 #pragma once
+#include <cstdint>
 
-typedef unsigned char u8;
-typedef signed char s8;
-typedef unsigned short u16;
-typedef signed short s16;
-typedef unsigned int u32;
-typedef signed int s32;
+typedef std::uint8_t u8;
+typedef std::int8_t s8;
+typedef std::uint16_t u16;
+typedef std::int16_t s16;
+typedef std::uint32_t u32;
+typedef std::int32_t s32;
 typedef float f32;
-typedef unsigned long int u64;
-typedef signed long int s64;
+typedef std::uint64_t u64;
+typedef std::int64_t s64;
 typedef double f64;
