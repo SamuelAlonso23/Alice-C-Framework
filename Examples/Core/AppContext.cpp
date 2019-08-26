e8136c8e9cd1f2af30679227a93be354c04787b3
AProgrammerFemale <54490976+AProgrammerFemale@users.noreply.github.com>
2019-08-26 05:48:13 +0200

    Add files via upload

---
+++ Examples/Core/AppContext.cpp
@@ -0,0 +1,24 @@
+#include <Core\AppContext.hpp>
+
+//The PID of the executing app
+u32 foo()
+{
+    return Alice::AppContext::GetApplicationID();
+}
+
+
+
+
+
+
+
+
+//If our executing app would be at location D:\Example\Program1.exe
+
+
+//This would show the full executing app location, so it would be equal to D:\Example\Program1.exe
+const char* ExecutingApp = ExecutableDirectory;
+
+//This would show the base of the full executing app location, so it would be equal to D:\Example\
+
+const char* BaseDir = BaseDirectory;
