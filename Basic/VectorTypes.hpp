c0a1da8b5ef4de001202bfbe6de6e05fe7fbf565
AProgrammerFemale <54490976+AProgrammerFemale@users.noreply.github.com>
2019-08-27 08:11:00 +0200

    Update VectorTypes.hpp

--- Basic/VectorTypes.hpp
+++ Basic/VectorTypes.hpp
@@ -2,7 +2,7 @@
 #pragma once
 #include <Basic\Types.hpp>
 
-typedef long long V128I __attribute__((vector_size(16), __aligned__(16)))
+typedef long long V128I __attribute__((vector_size(16), __aligned__(16)));
 typedef f32 V128F __attribute__((vector_size(16), __aligned__(16)));
 typedef f64 V128 __attribute__((vector_size(16), __aligned__(16)));
 typedef f32 V256F __attribute__((vector_size(32), __aligned__(32)));
