7f565e7dbad356738ef76074871f96e86a38a072
AProgrammerFemale <54490976+AProgrammerFemale@users.noreply.github.com>
2019-08-27 02:27:55 +0200

    Add files via upload

---
+++ Examples/System/CpuInfo.cpp
@@ -0,0 +1,19 @@
+#include <System\CpuInfo.hpp>
+
+void CpuInformation()
+{
+    Alice::System::GetCpuInfo();
+    //Check cpu features after calling GetCpuInfo
+    bool IsAdx = HasAdx();
+    bool IsAvx512f = HasAvx512f();
+}
+
+bool HasAdx()
+{
+    return Alice::System::ISA::Adx;
+}
+
+bool HasAvx512f()
+{
+    return Alice::System::ISA::Avx512f;
+}
