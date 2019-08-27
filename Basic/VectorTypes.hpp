898ed19d56718b9c49d91b55867c3201634b9849
AProgrammerFemale <54490976+AProgrammerFemale@users.noreply.github.com>
2019-08-27 08:10:29 +0200

    Add files via upload

--- Basic/VectorTypes.hpp
+++ Basic/VectorTypes.hpp
@@ -2,9 +2,10 @@
 #pragma once
 #include <Basic\Types.hpp>
 
-typedef f32 V128F __attribute__((vector_size(16)));
-typedef f64 V128 __attribute__((vector_size(16)));
-typedef f32 V256F __attribute__((vector_size(32)));
-typedef f64 V256 __attribute__((vector_size(32)));
-typedef f32 V512F __attribute__((vector_size(64)));
-typedef f64 V512 __attribute__((vector_size(64)));
+typedef long long V128I __attribute__((vector_size(16), __aligned__(16)))
+typedef f32 V128F __attribute__((vector_size(16), __aligned__(16)));
+typedef f64 V128 __attribute__((vector_size(16), __aligned__(16)));
+typedef f32 V256F __attribute__((vector_size(32), __aligned__(32)));
+typedef f64 V256 __attribute__((vector_size(32), __aligned__(32)));
+typedef f32 V512F __attribute__((vector_size(64), __aligned__(64)));
+typedef f64 V512 __attribute__((vector_size(64), __aligned__(64)));
