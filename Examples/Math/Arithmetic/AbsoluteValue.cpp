eeaf6586cda85c6040c0262dc837bbcf591f7151
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-08-31 12:22:59 +0200

    Add files via upload

---
+++ Examples/Math/Arithmetic/AbsoluteValue.cpp
@@ -0,0 +1,13 @@
+#include <Math\Arithmetic\AbsoluteValue.hpp>
+
+//returns 8
+s32 IntegerAbsoluteValue()
+{
+    return Alice::Math::Arithmetic::AbsoluteValue(-8);
+}
+
+//returns 8
+s32 IntegerAbsoluteValue2()
+{
+    return Alice::Math::Arithmetic::AbsoluteValue(8);
+}
