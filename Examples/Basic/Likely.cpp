2620d58293d16be4c48c7243ff28e179d2cd145b
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-01 14:06:41 +0200

    Add files via upload

---
+++ Examples/Basic/Likely.cpp
@@ -0,0 +1,19 @@
+#include <Basic\Likely.hpp>
+
+void LikelyTest()
+{
+    bool b = true;
+    if(AliceLikely(b))
+    {
+        //branch predictor will be omitted and the CPU will execute this code directly
+    }
+}
+
+void UnlikelyTest()
+{
+    bool b = true;
+    if(AliceUnlikely(!b))
+    {
+        //branch predictor will be omitted and the CPU will execute this code directly
+    }
+}
