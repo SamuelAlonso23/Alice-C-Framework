ac8a88fb95b2160de287a57f5f3224132d835616
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-01 16:29:05 +0200

    Add files via upload

--- Core/Forward.hpp
+++ Core/Forward.hpp
@@ -5,7 +5,12 @@
 
 namespace Alice
 {
-    template<class T> AliceInline T&& Forward(typename Alice::Concepts::RemoveReferences<T>&& value) noexcept
+    template<class T> AliceInline T&& Forward(Alice::Concepts::RemoveReferences<T>& value) noexcept
+    {
+        return static_cast<T&&>(value);
+    }
+
+    template<class T> AliceInline T&& Forward(Alice::Concepts::RemoveReferences<T>&& value) noexcept
     {
         return static_cast<T&&>(value);
     }
