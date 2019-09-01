8ec99c665ae41b41e13f4da139c198086d36ba02
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-01 10:01:47 +0200

    Add files via upload

---
+++ Examples/Math/Arithmetic/Maximum.cpp
@@ -0,0 +1,7 @@
+#include <Math\Arithmetic\Maximum.hpp>
+
+//returns 23.5
+f32 MaximumTest()
+{
+    Alice::Math::Arithmetic::Maximum(3.4f, 23.5f);
+}
---
+++ Examples/Math/Arithmetic/Minimum.cpp
@@ -0,0 +1,7 @@
+#include <Math\Arithmetic\Minimum.hpp>
+
+//returns 3.4
+f32 MinimumTest()
+{
+    return Alice::Math::Arithmetic::Minimum(23.5f, 3.4f);
+}
