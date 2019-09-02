5e2aa82d1b9a6e2ee7467b0cb73d3fff683faf0d
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-03 01:01:41 +0200

    Add files via upload

---
+++ Core/Lazy.hpp
@@ -0,0 +1,58 @@
+//Copyright Alice Framework, All Rights Reserved
+#pragma once
+#include <Basic\Likely.hpp>
+#include <Basic\Inline.hpp>
+
+namespace Alice
+{
+    template<class T> class Lazy
+    {
+    private:
+        T value = null;
+
+        bool valuecreated = false;
+    public:
+        AliceInline Lazy() const noexcept{}
+
+        AliceInline Lazy(T& object) noexcept : value(object), valuecreated(true){}
+
+        AliceInline Lazy(T&& object) noexcept : value(object), valuecreated(true){}
+
+        AliceInline Lazy(Lazy<T>& object) noexcept : value(object.value), valuecreated(true){}
+
+        AliceInline Lazy(Lazy<T>&& object) noexcept : value(object.value), valuecreated(true)
+        {
+            object.value = null;
+            object.valuecreated = false;
+        }
+
+        AliceInline void operator=(T& object) noexcept
+        {
+            value = object;
+            if(AliceUnlikely(!valuecreated))
+                valuecreated = true;
+        }
+
+        AliceInline void operator=(T&& object) noexcept
+        {
+            value = object;
+            if(AliceUnlikely(!valuecreated))
+                valuecreated = true;
+        }
+
+        AliceInline T& Object() const noexcept
+        {
+            return value;
+        }
+
+        AliceInline T Object() noexcept
+        {
+            return value;
+        }
+
+        AliceInline bool IsObjectCreated() const noexcept
+        {
+            return valuecreated;
+        }
+    };
+}
