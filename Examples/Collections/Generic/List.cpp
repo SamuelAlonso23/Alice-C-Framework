73fbcb1a66a73d61484cc42e7b0b836ddbd7f89a
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-04 02:46:30 +0200

    Add files via upload

--- Examples/Collections/Generic/List.cpp
+++ Examples/Collections/Generic/List.cpp
@@ -3,16 +3,21 @@
 
 void ListTest()
 {
-    Alice::Collections::Generic::List<f32> vec(4);//reserve 4 f32 at initialization in the memory
-    std::cout << std::boolalpha;
-    std::cout << "List length is: " << vec.Length() << std::endl
-    << "Is the list read only? " << vec.IsReadOnly() << std::endl;
-    vec[0] = 9.8f;
-    vec[1] = 4.3f;
-    vec[2] = 0.1f;
-    std::cout << "List capacity: " << vec.Capacity() << std::endl
-    << "First element: " << vec[0] << std::endl << "Second element: " << vec[1] << std::endl
-    << "Third element: " << vec[2] << std::endl << "Forth element: " << vec[3] << std::endl;
-    vec.Resize(2);
-    std::cout << "List capacity after resizing: " << vec.Capacity() << std::endl << "First element after resizing: " << vec[0] << std::endl << "Second element after resizing: " << vec[1] << std::endl;
+    Alice::Collections::Generic::List<f32> vec(4);
+    vec[0] = 2.4f;
+    vec[1] = 93847.9384f;
+    vec[2] = 9.0f;
+    vec.Add(92.4f);
+    std::cout << vec.Capacity() << std::endl
+    << vec.Size() << std::endl
+    << vec[0] << std::endl << vec[1] << std::endl
+    << vec[2] << std::endl << vec[3] << std::endl << vec[4] << std::endl;
+    vec.Remove();
+    std::cout << vec[4] << std::endl;
+    vec.Resize(vec.Size() + 3);
+    std::cout << vec.Capacity() << std::endl;
+    Alice::Collections::Generic::List<f32> vec2(Alice::Forward<decltype(vec)>(vec));
+    std::cout << vec2.Capacity() << std::endl << vec2[0] << std::endl << vec2[1] << std::endl << vec2[2] << std::endl;
+    char c;
+    std::cin >> c;
 }
