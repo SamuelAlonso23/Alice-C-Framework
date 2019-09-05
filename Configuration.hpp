8c24727e1c4768d693848dc1242bb1223d0c606d
John Patek <jpatek@ubiqsecurity.com>
2019-09-05 08:21:31 -0700

    Updated Configuration.hpp and CpuInfo.hpp to include posix compatibility

--- Configuration.hpp
+++ Configuration.hpp
@@ -1,4 +1,11 @@
 //Copyright Alice Framework, All Rights Reserved
 #pragma once
 
+#if defined(_WIN32)
 #define AliceWindows
+#elif defined(__APPLE__)
+#define AliceMac
+//Mac OS
+#elif defined(__linux__)
+#define AliceLinux
+#endif
