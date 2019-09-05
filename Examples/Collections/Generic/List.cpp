c535bdf5c761e22d0ed1af431765f5343eaa3130
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-05 08:02:35 +0200

    Add files via upload

--- Examples/Collections/Generic/List.cpp
+++ Examples/Collections/Generic/List.cpp
@@ -1,12 +1,19 @@
-#include <Collections\Generic\List.hpp>
+#include <Collections/Generic/List.hpp>
 #include <iostream>
 
 void ListTest()
 {
     Alice::Collections::Generic::List<f32> vec(4);
+    Alice::Collections::Generic::List<f32> vec3{0.2f, 2.3f, 3.4f};
     vec[0] = 2.4f;
     vec[1] = 93847.9384f;
     vec[2] = 9.0f;
+    std::cout << std::boolalpha;
+    std::cout << vec.Exists(2.4f) << std::endl;//returns true
+    std::cout << vec.BinarySearch(2.4f) << std::endl;//returns 0
+    vec3.Swap(vec);
+    std::cout << vec3.Exists(2.4f);//returns true
+    vec.Swap(vec3);
     vec.Add(92.4f);
     std::cout << vec.Capacity() << std::endl
     << vec.Size() << std::endl
