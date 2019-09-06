59204266d821e70ddd6c4ea007499cc2fd31b73a
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-06 05:10:17 +0200

    Add files via upload

--- Core/App.hpp
+++ Core/App.hpp
@@ -1,14 +1,27 @@
 //Copyright Alice Framework, All Rights Reserved
 #pragma once
+#include <Database.hpp>
+#include <Core/Exception.hpp>
 
 namespace Alice
-{
+{ 
     class App
     {
     public:
-        static void Init()
+        AliceInline App() noexcept
         {
-            
+            if(AppLoaded)
+                Exception::Raise(ExceptionType::AppAlreadyLoaded);
+            else
+            {
+                AppLoaded = true;
+
+            }
+        }
+
+        AliceInline ~App() noexcept
+        {
+
         }
     };
 }
