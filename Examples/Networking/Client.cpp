41f60b0ce77bf3d173fd950de0df8fc5605e36bb
John Patek <jpatek@ubiqsecurity.com>
2019-09-08 23:43:29 -0700

    Added main method to Server.cpp and Client.cpp

--- Examples/Networking/Client.cpp
+++ Examples/Networking/Client.cpp
@@ -1,2 +1,7 @@
 #include <Networking/Socket.hpp>
 
+int main(const int argc, const char ** argv)
+{
+    
+    return 0;
+}
--- Examples/Networking/Server.cpp
+++ Examples/Networking/Server.cpp
@@ -1,2 +1,6 @@
 #include <Networking/Socket.hpp>
 
+int main(const int argc, const char** argv)
+{
+    return 0;
+}
