176f472fab9002a80e83054d9b3ef9a17faad0f5
John Patek <jpatek@ubiqsecurity.com>
2019-09-09 00:26:02 -0700

    Client example mostly finished but a major restructure is in order

--- Examples/Networking/Client.cpp
+++ Examples/Networking/Client.cpp
@@ -1,7 +1,30 @@
+#include <Basic/ExceptionType.hpp>
+#include <Core/Exception.hpp>
 #include <Networking/Socket.hpp>
+#include <cstdlib>
+#include <cstring>
 
 int main(const int argc, const char ** argv)
 {
-    
-    return 0;
+    char *Message = "Hello from client";
+     
+    Alice::Networking::Socket ClientSocket(
+        Alice::Networking::SocketDomain::IPv4, 
+        Alice::Networking::SocketType::Stream);
+       
+    if(Alice::Exception::Check(ExceptionType::SocketOpen))
+    {
+        return EXIT_FAILURE;
+    }
+
+    ClientSocket.Connect("127.0.0.1",8080);
+
+    if(Alice::Exception::Check(ExceptionType::SocketConnect))
+    {
+        return EXIT_FAILURE;
+    }
+
+    ClientSocket.Write(ClientSocket, Message, strlen(Messgae));
+
+    return EXIT_SUCCESS;
 }
