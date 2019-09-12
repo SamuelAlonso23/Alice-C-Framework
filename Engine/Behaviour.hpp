f86e6d662eced3cb46a92a786b18245cc5970fac
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-12 15:54:49 +0200

    Add files via upload

---
+++ Engine/Behaviour.hpp
@@ -0,0 +1,36 @@
+//Copyright Alice Framework, All Rights Reserved
+#pragma once
+#include <Basic/Inline.hpp>
+
+namespace Alice
+{
+    namespace Engine
+    {
+        namespace Behaviour
+        {
+            AliceInline void Awake() noexcept
+            {
+                //Add here your very beggining code
+
+            }
+
+            AliceInline void Start() noexcept
+            {
+                //Add here your start-up code
+
+            }
+
+            AliceInline void Update() noexcept
+            {
+                //Add here the code that must be updated each frame
+
+            }
+
+            AliceInline void Finish() noexcept
+            {
+                //Add here your code that will run before the application ends
+            
+            }
+        }
+    }
+}
