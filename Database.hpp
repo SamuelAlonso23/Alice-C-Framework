d12f9ba5bd39a32b3276f72a6c5e26167fd6514a
verodev23 <54490976+AProgrammerFemale@users.noreply.github.com>
2019-09-12 15:55:46 +0200

    Add files via upload

--- Database.hpp
+++ Database.hpp
@@ -2,10 +2,18 @@
 #pragma once
 #include <OS.hpp>
 
-bool AppLoaded = false;
+bool AppLoaded = false, Exit = false;
+
+u16 MainWindowX = 0, MainWindowY = 0, MainWindowWidth = 100, MainWindowHeight = 100;
+
 const char* BaseDirectory = nullptr;
+
 const char* ExecutableDirectory = nullptr;
 
 #if defined(AliceWindows)
+LPCWSTR MainWindowTitle = L"AliceMainWindow";
+
 HINSTANCE AppInstance = nullptr;
+
+HWND MainWindowHandler = nullptr;
 #endif
